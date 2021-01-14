df61869dd41e9648dbffc4cdc805557aade70ce5
Kenan Jasim <kenanabid10@gmail.com>
2021-01-14 16:05:19 +0000

    add the host details

--- cmd/nenvn-api/app/cmd/commands.go
+++ cmd/nenvn-api/app/cmd/commands.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"nenvoy.com/cmd/nenvn-api/app/pkg/details"
 )
 
@@ -23,6 +24,42 @@ func getHosts(w http.ResponseWriter, r *http.Request) {
 
 func getNetworks(w http.ResponseWriter, r *http.Request) {
 	resp, err := details.GetNetworks()
+
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("Error collecting details"))
+	}
+
+	// Write the application type headers
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	w.WriteHeader(200)
+	w.Write(resp)
+}
+
+func getHost(w http.ResponseWriter, r *http.Request) {
+
+	// Get the variables
+	vars := mux.Vars(r)
+	resp, err := details.GetHost(vars["host"])
+
+	if err != nil {
+		w.WriteHeader(400)
+		w.Write([]byte("Error collecting details"))
+	}
+
+	// Write the application type headers
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	w.WriteHeader(200)
+	w.Write(resp)
+}
+
+func getHostIP(w http.ResponseWriter, r *http.Request) {
+
+	// Get the variables
+	vars := mux.Vars(r)
+	resp, err := details.GetHostIP(vars["host"])
 
 	if err != nil {
 		w.WriteHeader(400)
--- cmd/nenvn-api/app/cmd/server.go
+++ cmd/nenvn-api/app/cmd/server.go
@@ -6,14 +6,22 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// StartServer - Starts the server with all the routing in place
 func StartServer() {
 	r := mux.NewRouter()
+
 	// Handle the getting of the host details
-	r.HandleFunc("/details/hosts", getHosts)
+	r.HandleFunc("/hosts", getHosts)
 
 	// Handle the getting of the network details
-	r.HandleFunc("/details/networks", getNetworks)
-	// http.Handle("/", r)
+	r.HandleFunc("/networks", getNetworks)
+
+	// Get the IP of a particular host
+	r.HandleFunc("/details/{host}", getHost)
+
+	// Get the IP of a particular host
+	r.HandleFunc("/details/{host}/ipv4", getHostIP)
+
 	http.ListenAndServe(":8000", r)
 
 }
